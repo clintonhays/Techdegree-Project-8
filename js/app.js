3982c08cdde6bad722c2b239052494e33c55a2ba
clintonhays <clintonhays84@gmail.com>
2020-10-04 13:06:21 +0900

    add modal scroll functionality

--- js/app.js
+++ js/app.js
@@ -10,7 +10,6 @@ const urlAPI = 'https://randomuser.me/api/?results=12&inc=name,location,email,ph
 const directory = document.querySelector('.directory');
 const directoryWrapper = document.getElementById('directory-wrapper');
 const overlay = document.querySelector('.overlay');
-const modal = document.getElementById('modal');
 const modalContent = document.querySelector('.modal-content');
 const modalClose = document.querySelector('.close-modal');
 const scrollRight = document.querySelector('.scroll-right');
@@ -75,7 +74,6 @@ function displayModal (index) {
     `;
 
 	overlay.classList.remove('hidden');
-	modal.classList.add(`data-index="${index}"`);
 	modalContent.innerHTML = modalHTML;
 }
 
@@ -100,12 +98,24 @@ search.addEventListener('keyup', (e) => {
 directoryWrapper.addEventListener('click', (e) => {
 	if (e.target !== directoryWrapper) {
 		const card = e.target.closest('.card');
-		const index = card.getAttribute('data-index');
+		let index = card.getAttribute('data-index');
 
 		displayModal(index);
 
+		let currentIndex = index;
+
 		scrollRight.addEventListener('click', () => {
-			console.log(index + 1);
+			if (currentIndex < employees.length - 1) {
+				currentIndex++;
+				displayModal(currentIndex);
+			}
+		});
+
+		scrollLeft.addEventListener('click', () => {
+			if (currentIndex > 0) {
+				currentIndex--;
+				displayModal(currentIndex);
+			}
 		});
 	}
 });
@@ -114,8 +124,6 @@ modalClose.addEventListener('click', () => {
 	overlay.classList.add('hidden');
 	modal.removeAttribute('class');
 });
-
-scrollRight.addEventListener('click', (e) => {});
 
 // closeTitle.addEventListener('click', () => {
 // 	titleScreen.classList.add('hidden');
